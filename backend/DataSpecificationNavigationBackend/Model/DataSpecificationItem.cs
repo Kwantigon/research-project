b7ad592569290c4035377fa2b168897be781beac
Quoc Anh Nguyen <nquocanh97@gmail.com>
2025-07-27 16:12:18 +0200

    Added List<Message> to DataSpecificationItem.
    This will make the relationship between Message and DataSpecificationItem many-to-many. Until now, it has been one-to-many, which is unintentional.

--- backend/DataSpecificationNavigationBackend/Model/DataSpecificationItem.cs
+++ backend/DataSpecificationNavigationBackend/Model/DataSpecificationItem.cs
@@ -16,6 +16,8 @@ public class DataSpecificationItem
 
 	public string? Comment { get; set; }
 
+	public virtual List<Message>? Messages { get; set; }
+
 	public int DataSpecificationId { get; set; }
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
